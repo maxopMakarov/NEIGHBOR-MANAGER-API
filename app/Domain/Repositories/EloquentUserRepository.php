5a987316babe901e2ebd996f9746077e5c890eea
maxopMakarov <maksim.makarov1994@gmail.com>
2025-09-28 21:59:21 +0200

    Add user management commands, queries, and repository implementation

--- app/Domain/Repositories/EloquentUserRepository.php
+++ app/Domain/Repositories/EloquentUserRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Repositories;
+namespace App\Domain\Repositories;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -19,15 +19,11 @@ class EloquentUserRepository implements UserRepositoryInterface
 
     public function create(array $data): User
     {
-        $data['password'] = Hash::make($data['password']);
         return User::create($data);
     }
 
     public function update(User $user, array $data): User
     {
-        if (isset($data['password'])) {
-            $data['password'] = Hash::make($data['password']);
-        }
         $user->update($data);
         return $user;
     }
